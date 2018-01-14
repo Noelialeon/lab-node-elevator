c035807c477327645d6c22ef7310e496eb8b0ebe
Noelia <noelials@hotmail.es>
2018-01-14 21:53:44 +0100

    people enter and leave the elevator

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,13 +1,17 @@
 class Elevator {
   constructor(){
     this.floor      = 0;
-    this.MAXFLOOR   = 10;
+    this.MAXFLOOR   = 5;
+    this.waitingList = [];
+    this.passengers = [];
     this.requests   = [];
     this.direction = 'up';
   }
 
   start() {
-    let updateElevator = setInterval(update(), 1000);
+    setInterval(() => {
+      this.update();
+    }, 1000);
   }
 
   stop() {
@@ -15,55 +19,66 @@ class Elevator {
   }
 
   update() {
+    switch (this.direction) {
+      case 'up':
+      this.floorUp();
+      break;
+      case 'down':
+      this.floorDown();
+      break;
+    };
     this.log();
+    this._passengersEnter();
+    this._passengersLeave();
   }
   
   _passengersEnter() {
-
-  }
+    this.passengersInActualFloor = this.waitingList.filter(passengers => passengers.originFloor === this.floor);
+    this.passengers.push(...this.passengersInActualFloor);
+    this.passangersOnRequest = this.passengersInActualFloor.map(passengers => passengers.destinationFloor)
+    this.requests.push(...this.passangersOnRequest);
+    this.waitingList = this.waitingList.filter(waitingPassengers => waitingPassengers.originFloor !== this.floor);
+    if(this.passengersInActualFloor.length !== 0){
+    console.log(`${this.passengersInActualFloor.map(passengers => passengers.name)} has enter the elevator`);
+  };
+}
   
   _passengersLeave() {
-
+    this.passengersLeaving = this.passengers.filter(passengers => passengers.destinationFloor === this.floor);
+    this.passengers = this.passengers.filter(actualPassengers => actualPassengers.destinationFloor !== this.floor);
+    if(this.passengersLeaving.length !== 0){
+    console.log(`${this.passengersLeaving.map(passengers => passengers.name)} has left the elevator`);
+    };
   }
   
   floorUp() {
     if (this.floor < this.MAXFLOOR){
-     this.floor++;
-     this.log();     
+     this.floor++;   
     } else {
-      console.log("Max Floor reached")
-     this.log();           
+      this.direction = 'down';         
+      this.floor--; 
     }
   }
   
   floorDown() {
     if (this.floor > 0){
       this.floor--;
-      this.log();
     } else {
-      console.log("Min Floor reached")
-     this.log();           
+      this.direction = 'up';
+      this.floor++; 
     }
   }
 
-  // call(name, originFloor, destinationFloor) {
-  //   let person = new Person(name, originFloor, destinationFloor);
-  //   this.requests.push(person);
-  // }
+  call(person) {
+    this.requests.push(person.originFloor);
+    this.waitingList.push(person);
+    console.log(`${person.name} called from ${person.originFloor}`)
+  }
 
   log() {
-    console.log(`Direction:${this.direction} | Floor: ${this.floor}`)
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor} | Passengers: ${this.passengers.map(passengers => passengers.name).join(', ')}`)
   }
 }
 
-let mainElevator = new Elevator();
-
-
-
-mainElevator.floorUp();
-// mainElevator.call("Maria", 1, 3);
-console.log(mainElevator.requests);
-mainElevator.floorUp();
-mainElevator.floorUp();
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,2 +1,15 @@
 const Person = require('./person.js');
 const Elevator = require('./elevator.js');
+
+let mainElevator = new Elevator();
+
+
+let ana = new Person("Ana", 2, 5);
+let oriol = new Person("Oriol", 3, 5);
+// let belen = new Person("Belén", 1, 4);
+// let lola = new Person("Lola", 1, 5);
+
+
+mainElevator.start();
+mainElevator.call(ana);
+mainElevator.call(oriol);
